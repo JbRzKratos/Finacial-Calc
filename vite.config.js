182469e948086fdb630b5ac360cb66e691729ff1
JbRzKratos <gokul30sep@gmail.com>
2026-05-22 19:49:13 +0530

    Fixed Some Browser Issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,20 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'remove-crossorigin-from-css',
+      transformIndexHtml(html) {
+        // Strip the crossorigin attribute specifically from rel="stylesheet" link tags
+        // Rolldown/Vite renders it as <link rel="stylesheet" crossorigin href="...css">
+        return html.replace(
+          /<link rel="stylesheet" crossorigin href="(.*?)">/g,
+          '<link rel="stylesheet" href="$1">'
+        );
+      },
+    },
+  ],
   base: '/',
   resolve: {
     alias: {
